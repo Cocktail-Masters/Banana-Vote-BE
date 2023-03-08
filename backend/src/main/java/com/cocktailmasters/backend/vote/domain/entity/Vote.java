b34e5861e42a96ffa61332f8b9bc3cf98638fd60
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-08 15:22:43 +0900

    feat: Vote entity 생성 및 필드 추가

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/domain/entity/Vote.java
@@ -0,0 +1,49 @@
+package com.cocktailmasters.backend.vote.domain.entity;
+
+import com.cocktailmasters.backend.common.domain.entity.BaseEntity;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "vote_id"))
+public class Vote extends BaseEntity {
+
+    @NotNull
+    private String voteTitle;
+
+    private String voteContent;
+
+    private String voteImageUrl;
+
+    private String voteThumbnailUrl;
+
+    @Builder.Default
+    private Long voteHits = 0L;
+
+    private LocalDateTime voteEndDate;
+
+    @Builder.Default
+    private int voteReportedNumber = 0;
+
+    @NotNull
+    @Builder.Default
+    private boolean isEvent = false;
+
+    @NotNull
+    private boolean isAnonymous;
+
+    @NotNull
+    private boolean isPublic;
+
+    @NotNull
+    @Builder.Default
+    private boolean isClosed = false;
+}
