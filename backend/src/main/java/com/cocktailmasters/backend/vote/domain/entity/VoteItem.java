e9f6130dc115951b502d264108319f6a7e00ec34
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-08 15:34:31 +0900

    feat: VoteItem entity 생성 및 필드 추가

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/domain/entity/VoteItem.java
@@ -0,0 +1,30 @@
+package com.cocktailmasters.backend.vote.domain.entity;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "vote_item_id"))
+public class VoteItem {
+
+    @NotNull
+    private String voteItemTitle;
+
+    private String iframeLink;
+    private String voteItemImageUrl;
+
+    @NotNull
+    private int voteItemNumber;
+
+    @Builder.Default
+    private Long totalPoints = 0L;
+
+    @Builder.Default
+    private int votedNumber = 0;
+}
