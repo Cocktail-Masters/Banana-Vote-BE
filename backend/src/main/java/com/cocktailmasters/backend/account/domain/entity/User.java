888c708457921af43bf398b9c617504d35b8c534
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-09 18:05:59 +0900

    feat: User entity 필드 추가

--- backend/src/main/java/com/cocktailmasters/backend/account/domain/entity/User.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/domain/entity/User.java
@@ -1,17 +1,34 @@
 package com.cocktailmasters.backend.account.domain.entity;
 
+import com.cocktailmasters.backend.common.domain.entity.BaseEntity;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
 
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "user_id"))
-public class User {
+public class User extends BaseEntity {
+
+    @NotNull
+    private String nickname;
+
+    private String gender;
+
+    @Builder.Default
+    private Boolean isAdmin = false;
+
+    @Builder.Default
+    private Long points = 0L;
+
+    private LocalDateTime banReleaseDate;
+
+    @Deprecated
+    private int ban_number = 0;
 }
