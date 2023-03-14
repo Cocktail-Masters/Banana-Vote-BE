399c64953baf0144d0ee7c2ad247b998af122b82
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-14 22:51:39 +0900

    feat: BaseEntity 상속

--- backend/src/main/java/com/cocktailmasters/backend/advertising/domain/entity/BannerAdvertisement.java
+++ backend/src/main/java/com/cocktailmasters/backend/advertising/domain/entity/BannerAdvertisement.java
@@ -1,7 +1,9 @@
 package com.cocktailmasters.backend.advertising.domain.entity;
 
+import com.cocktailmasters.backend.common.domain.entity.BaseEntity;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -14,7 +16,8 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @AttributeOverride(name = "id", column = @Column(name = "banner_advertisement_id"))
-public class BannerAdvertisement {
+@Entity
+public class BannerAdvertisement extends BaseEntity {
 
     @NotNull
     private String bannerImageUrl;
