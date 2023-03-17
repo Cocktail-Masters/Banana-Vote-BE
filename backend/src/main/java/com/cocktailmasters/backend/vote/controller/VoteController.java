df2e2b30101064fbd847d7f181647588cb30005a
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-17 14:32:35 +0900

    feat: Vote Controller 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
@@ -0,0 +1,10 @@
+package com.cocktailmasters.backend.vote.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/votes")
+public class VoteController {
+}
