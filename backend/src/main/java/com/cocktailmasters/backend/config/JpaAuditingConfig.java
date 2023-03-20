43e4ab2a7b5a4b4f4dadd45c7cd7b10074b565a4
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-20 15:25:07 +0900

    feat: JpaAuditingConfig 추가

---
+++ backend/src/main/java/com/cocktailmasters/backend/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.cocktailmasters.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
