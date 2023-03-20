2c2e9b6173b024411a317ed63b538d4bcc42c709
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-20 14:53:42 +0900

    feat: VoteController mapping URL 변경

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/votes")
+@RequestMapping("/votes")
 public class VoteController {
 }
