20b4e1300fda6c5bab98a254293a5d2bb475d7f1
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-22 16:28:15 +0900

    feat: add season repository

---
+++ backend/src/main/java/com/cocktailmasters/backend/season/domain/repository/SeasonRepository.java
@@ -0,0 +1,7 @@
+package com.cocktailmasters.backend.season.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cocktailmasters.backend.season.domain.entity.Season;
+
+public interface SeasonRepository extends JpaRepository<Season, Long> { }
