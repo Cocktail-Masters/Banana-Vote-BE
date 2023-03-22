251b6d3adbff7a893e3f9310080f8ea43cc4ca48
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-22 15:01:50 +0900

    feat: findByTagName query method 추가

--- backend/src/main/java/com/cocktailmasters/backend/common/domain/repository/TagRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/common/domain/repository/TagRepository.java
@@ -4,4 +4,6 @@ import com.cocktailmasters.backend.common.domain.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Tag findByTagName(String tagName);
 }
