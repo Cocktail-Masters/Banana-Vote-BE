e6619a9dadc03dd2b33ffa05a0972eca45c642cd
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-22 15:02:27 +0900

    feat: createVote post api 추가

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
@@ -1,10 +1,25 @@
 package com.cocktailmasters.backend.vote.controller;
 
-
+import com.cocktailmasters.backend.vote.controller.dto.CreateVoteRequest;
+import com.cocktailmasters.backend.vote.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/votes")
 public class VoteController {
+
+    private final VoteService voteService;
+
+    @PostMapping("")
+    public ResponseEntity<String> createVote(Long userId,
+                                             CreateVoteRequest createVoteRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(voteService.createVote(userId, createVoteRequest));
+    }
 }
