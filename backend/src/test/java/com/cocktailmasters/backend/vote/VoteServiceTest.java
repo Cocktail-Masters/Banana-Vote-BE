05dd34fad2bee2d5f729ff6d86eb07c2a80346ef
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-22 16:20:58 +0900

    feat: voteServiceTest 생성

---
+++ backend/src/test/java/com/cocktailmasters/backend/vote/VoteServiceTest.java
@@ -0,0 +1,7 @@
+package com.cocktailmasters.backend.vote;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+
+@AutoConfigureWebMvc
+public class VoteServiceTest {
+}
