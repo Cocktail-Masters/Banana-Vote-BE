50f86811c12ebc1285712b021bc33d0e9635934d
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-23 17:22:26 +0900

    refactor: erase unused import

--- backend/src/main/java/com/cocktailmasters/backend/season/controller/SeasonController.java
+++ backend/src/main/java/com/cocktailmasters/backend/season/controller/SeasonController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
