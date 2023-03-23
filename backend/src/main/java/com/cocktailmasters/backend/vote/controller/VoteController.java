bbeb28c75986f371270d6ae1c800eeee3126973d
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-23 14:56:49 +0900

    feat: swagger tag 추가

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
@@ -2,6 +2,8 @@ package com.cocktailmasters.backend.vote.controller;
 
 import com.cocktailmasters.backend.vote.controller.dto.CreateVoteRequest;
 import com.cocktailmasters.backend.vote.service.VoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "vote", description = "투표 관리")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/votes")
@@ -16,15 +19,15 @@ public class VoteController {
 
     private final VoteService voteService;
 
+    @Operation(summary = "투표 생성", description = "새로운 투표 생성")
     @PostMapping("")
     public ResponseEntity<String> createVote(Long userId,
                                              CreateVoteRequest createVoteRequest) throws Exception {
-        //TODO: 사용자 검사 및 입력 값 검사
+        //TODO: 사용자 검사
         if (voteService.createVote(userId, createVoteRequest)) {
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body("");
         }
-        //TODO: 예외 특정
         throw new Exception();
     }
 }
