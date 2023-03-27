90f69b0e0027452123eb81749db28716b72244e0
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-27 17:57:18 +0900

    feat: add ranking dtos

---
+++ backend/src/main/java/com/cocktailmasters/backend/season/controller/dto/item/UserRanking.java
@@ -0,0 +1,29 @@
+package com.cocktailmasters.backend.season.controller.dto.item;
+
+import com.cocktailmasters.backend.season.domain.entity.SeasonRanking;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRanking {
+    @JsonProperty("user_id")
+    private long userId;
+
+    private long ranking;
+
+    private String nickname;
+
+    private long score;
+
+    public UserRanking(SeasonRanking seasonRanking) {
+        this.userId = seasonRanking.getId();
+        this.ranking = seasonRanking.getRanking();
+        this.nickname = seasonRanking.getUser().getNickname();
+        this.score = seasonRanking.getScore();
+    }
+}
