41acc7b99373a6d74f10e86adc285e2f2a2a740d
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-27 16:29:31 +0900

    feat: 투표 글 상세보기 함수 추가

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/VoteController.java
@@ -1,15 +1,14 @@
 package com.cocktailmasters.backend.vote.controller;
 
 import com.cocktailmasters.backend.vote.controller.dto.CreateVoteRequest;
+import com.cocktailmasters.backend.vote.controller.dto.FindVoteDetailResponse;
 import com.cocktailmasters.backend.vote.service.VoteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "vote", description = "투표 관리")
 @RequiredArgsConstructor
@@ -25,9 +24,17 @@ public class VoteController {
                                              CreateVoteRequest createVoteRequest) throws Exception {
         //TODO: 사용자 검사
         if (voteService.createVote(userId, createVoteRequest)) {
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body("");
+            return ResponseEntity.created(null).build();
         }
         throw new Exception();
+    }
+
+    @Operation(summary = "투표글 상세보기", description = "투표글 상세보기")
+    @GetMapping("/{vote_id}")
+    public ResponseEntity<FindVoteDetailResponse> findVoteDetail(Long userId,
+                                                                 @PathVariable("vote_id") Long voteId) {
+        //TODO: 사용자 검사
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(voteService.findVoteDetail(voteId));
     }
 }
