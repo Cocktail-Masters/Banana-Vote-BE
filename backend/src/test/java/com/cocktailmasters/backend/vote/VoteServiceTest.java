510ca9125f206a9124da34ce2a9a9f62e4688b09
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-27 16:33:19 +0900

    feat: 테스트 코드 삭제

--- backend/src/test/java/com/cocktailmasters/backend/vote/VoteServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.cocktailmasters.backend.vote;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-
-@AutoConfigureWebMvc
-public class VoteServiceTest {
-}
