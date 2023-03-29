a203e20942223a1c79630beecec8cb0131a51f4a
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-30 02:15:18 +0900

    feat: add function on Ranking repository
    유저 순위 계산을 위한 쿼리 추가
    닉네임으로 스코어 반환 함수 추가
    페이징을 위한 SeasonRanking 엔티티 반환 함수 추가

--- backend/src/main/java/com/cocktailmasters/backend/season/domain/repository/RankingRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/season/domain/repository/RankingRepository.java
@@ -1,13 +1,19 @@
 package com.cocktailmasters.backend.season.domain.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.cocktailmasters.backend.season.domain.entity.SeasonRanking;
 
-public interface RankingRepository extends JpaRepository<SeasonRanking, Long> {
+public interface RankingRepository extends PagingAndSortingRepository<SeasonRanking, Long> {
     long countBySeasonId(long seasonId);
-    List<SeasonRanking> findBySeasonId(long seasonId, Pageable pageable);
+
+    long findScoreBySeasonIdAndNickname(long seasonId, String nickname);
+
+    Page<SeasonRanking> findBySeasonIdOrderByScoreDesc(long seasonId, Pageable pageable);
+
+    @Query(value = "SELECT COUNT(*) + 1 FROM season_ranking WHERE season_id = :seasonId AND score > :score", nativeQuery = true)
+    long getUserRankingByScore(long seasonId, long score);
 }
