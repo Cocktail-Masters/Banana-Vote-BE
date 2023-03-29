c5a73b2e436d42afa6cdc14f12aeb53badff83f8
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-30 02:19:46 +0900

    feat: update getRanking API
    추가 된 Custom Excpetion으로 page, pageSize, seasonID 예외 처리 추가
    
    다른 API와 통합하면서 추가된 nickname 쿼리 파라미터 반영

--- backend/src/main/java/com/cocktailmasters/backend/season/controller/RankingController.java
+++ backend/src/main/java/com/cocktailmasters/backend/season/controller/RankingController.java
@@ -1,8 +1,6 @@
 package com.cocktailmasters.backend.season.controller;
 
-import java.security.InvalidParameterException;
-import java.util.List;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cocktailmasters.backend.season.controller.dto.RankingResponse;
-import com.cocktailmasters.backend.season.controller.dto.item.UserRanking;
 import com.cocktailmasters.backend.season.service.RankingService;
+import com.cocktailmasters.backend.util.exception.CustomException;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,18 +27,27 @@ public class RankingController {
     private final String DEFAULT_PAGE_SIZE = "10";
 
     @Operation(summary = "랭킹 정보 조회",
-        description = "일치된 시즌의 페이지에 해당하는 랭킹 정보 리스트 반환")
+        description = "일치된 시즌의 페이지에 해당하는 랭킹 정보 리스트 반환, 닉네임을 포함 할 경우 페이지 파라미터는 무효되고, 페이지 사이즈에 맞추어 해당 플레이어가 포함된 결과 반환")
     @GetMapping("/{seasonId}")
     public ResponseEntity<RankingResponse> getRanking(@PathVariable long seasonId,
             @RequestParam(required = false, name = "page", defaultValue = "0") int page,
-            @RequestParam(required = false, name = "size", defaultValue = DEFAULT_PAGE_SIZE) int pageSize) {
-            // invalid query parameter
-            if(page < 0 || pageSize <= 0)
-                throw new InvalidParameterException("invalid page or page size");
+            @RequestParam(required = false, name = "size", defaultValue = DEFAULT_PAGE_SIZE) int pageSize,
+            @RequestParam(required = false, name = "nickname") String nickname) {
+            if(!rankingService.isValidSeason(seasonId)) // check season is existed
+                throw new CustomException(HttpStatus.BAD_REQUEST, "invalid season id");    
+            if(page < 0 || pageSize < 1)
+                throw new CustomException(HttpStatus.BAD_REQUEST, "bad page or page size request");
 
-            long totalPages = rankingService.getRankingTotalPages(seasonId, pageSize);
-            List<UserRanking> userRankings = rankingService.getRankingWithPage(seasonId, page, pageSize);
-        
-            return ResponseEntity.ok(new RankingResponse(totalPages, userRankings));
+            if(nickname != null && !nickname.isBlank()) {
+                // TODO : 해당 닉네임을 가진 유저가 존재하는지 확인하는 로직 필요 user Repositoy에서 가져다 쓸 것(?)
+                page = rankingService.getRankingPageNumberByNickname(seasonId, pageSize, nickname);
+            }
+            
+            RankingResponse rankingResponse = rankingService.getRankingListWithPage(seasonId, page, pageSize);
+
+            if(rankingResponse.getRankingList() == null || rankingResponse.getRankingList().isEmpty())
+                return ResponseEntity.noContent().build();
+            else
+                return ResponseEntity.ok().body(rankingResponse);
     }
 }
