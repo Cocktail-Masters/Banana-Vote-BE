05bb5be015046a512d5e72c3ff3b23750d5e3fe9
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-29 21:33:10 +0900

    feat: FindVotesResponse dto 추가

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/FindVotesResponse.java
@@ -0,0 +1,15 @@
+package com.cocktailmasters.backend.vote.controller.dto;
+
+import com.cocktailmasters.backend.vote.controller.dto.item.VoteDto;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class FindVotesResponse {
+
+    private Long totalCount;
+    private List<VoteDto> votes;
+}
