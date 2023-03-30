57549b05eaa35dda1ba9bfff4b982d353fe3ea29
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-03-30 17:42:12 +0900

    feat: add point log repository
    사용자 아이디로 모든 포인트 로그 조회

---
+++ backend/src/main/java/com/cocktailmasters/backend/point/domain/repository/PointLogRepository.java
@@ -0,0 +1,11 @@
+package com.cocktailmasters.backend.point.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cocktailmasters.backend.point.domain.entity.PointLog;
+
+public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+    List<PointLog> findAllByUserId(long userId);
+}
