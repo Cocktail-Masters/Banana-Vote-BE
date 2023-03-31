a9368fb3ca99fd7ee06fc11c165ede8f401e0d64
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-31 23:17:11 +0900

    feat: createVoteItemDto 전역 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/item/VoteItemDto.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/item/VoteItemDto.java
@@ -1,5 +1,6 @@
 package com.cocktailmasters.backend.vote.controller.dto.item;
 
+import com.cocktailmasters.backend.vote.domain.entity.VoteItem;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,4 +14,15 @@ public class VoteItemDto {
     private String imageUrl;
     private Long totalPoints;
     private int votedNumber;
+
+    public static VoteItemDto createVoteItemDto(VoteItem voteItem) {
+        return VoteItemDto.builder()
+                .itemNumber(voteItem.getVoteItemNumber())
+                .title(voteItem.getVoteItemTitle())
+                .iframeLink(voteItem.getIframeLink())
+                .imageUrl(voteItem.getVoteItemImageUrl())
+                .totalPoints(voteItem.getTotalPoints())
+                .votedNumber(voteItem.getVotedNumber())
+                .build();
+    }
 }
