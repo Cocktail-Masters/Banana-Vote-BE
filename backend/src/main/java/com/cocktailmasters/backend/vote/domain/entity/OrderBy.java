9c0feab6cf1f120ab90ef8215e8be59ef4380879
swoody1101@gmail.com <swoody1101@gmail.com>
2023-03-31 18:06:19 +0900

    feat: OrderBy enum 추가

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/domain/entity/OrderBy.java
@@ -0,0 +1,33 @@
+package com.cocktailmasters.backend.vote.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public enum OrderBy {
+
+    LATEST(1, "created_date", "최신순"),
+    POPULARITY(2, "voted_number", "참여순"),
+    HITS(3, "vote_hits", "조회순"),
+    OPINION(4, "opinion_number", "댓글순");
+
+    private final int number;
+    private final String value;
+    private final String title;
+
+    private static final Map<Integer, OrderBy> BY_NUMBER = new HashMap<>();
+
+    static {
+        for (OrderBy e : values()) {
+            BY_NUMBER.put(e.number, e);
+        }
+    }
+
+    public static String valueOfNumber(int number) {
+        return BY_NUMBER.get(number).getValue();
+    }
+}
