94af86ae28667c006a96e1455d66318654dc403a
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-04 16:24:22 +0900

    feat: createTags 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/common/service/TagService.java
+++ backend/src/main/java/com/cocktailmasters/backend/common/service/TagService.java
@@ -1,8 +1,25 @@
 package com.cocktailmasters.backend.common.service;
 
+import com.cocktailmasters.backend.common.contorller.dto.CreateTagsRequest;
+import com.cocktailmasters.backend.common.domain.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
+@RequiredArgsConstructor
 @Service
 public class TagService {
+
+    private final TagRepository tagRepository;
+
+    public boolean createTags(CreateTagsRequest createTagsRequest) {
+        //TODO: 관리자 검증
+        for (String tagName : createTagsRequest.getTags()) {
+            if (tagRepository.findByTagName(tagName).isEmpty()) {
+                createTagsRequest.getTags().remove(tagName);
+            }
+        }
+        tagRepository.saveAll(createTagsRequest.toTagEntity());
+        return true;
+    }
 }
