1bf414a77c6b4fd2dff7fce1ab319057072169b4
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-06 01:05:12 +0900

    refactor: apply review

--- backend/src/main/java/com/cocktailmasters/backend/point/domain/repository/PointLogRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/point/domain/repository/PointLogRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cocktailmasters.backend.point.domain.entity.PointLog;
 
 public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+
     List<PointLog> findAllByUserId(long userId);
 }
