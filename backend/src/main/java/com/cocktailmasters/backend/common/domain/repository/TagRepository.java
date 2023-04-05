37037cfc82787bc5167137dafa59406ae36cc3af
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-05 15:14:29 +0900

    feat: findTop10ByLastModifiedDateBetweenOrderByTagUsedNumber 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/common/domain/repository/TagRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/common/domain/repository/TagRepository.java
@@ -3,9 +3,13 @@ package com.cocktailmasters.backend.common.domain.repository;
 import com.cocktailmasters.backend.common.domain.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
     Optional<Tag> findByTagName(String tagName);
+
+    List<Tag> findTop10ByLastModifiedDateBetweenOrderByTagUsedNumber(LocalDate start, LocalDate end);
 }
