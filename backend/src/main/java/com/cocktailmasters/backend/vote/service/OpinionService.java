60cf60b61eb6ed5bd5154f205edf09fd89a4283f
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-05 18:03:10 +0900

    feat: OpinionService 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/service/OpinionService.java
@@ -0,0 +1,9 @@
+package com.cocktailmasters.backend.vote.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OpinionService {
+}
