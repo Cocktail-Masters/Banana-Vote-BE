aa01adce8db21086224a14864c959906587161ac
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-05 15:13:34 +0900

    feat: createTags get api 생성

--- backend/src/main/java/com/cocktailmasters/backend/common/contorller/TagController.java
+++ backend/src/main/java/com/cocktailmasters/backend/common/contorller/TagController.java
@@ -1,15 +1,16 @@
 package com.cocktailmasters.backend.common.contorller;
 
 import com.cocktailmasters.backend.common.contorller.dto.CreateTagsRequest;
+import com.cocktailmasters.backend.common.contorller.dto.FindTop10TagsResponse;
 import com.cocktailmasters.backend.common.service.TagService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @Tag(name = "tag", description = "태그 관리")
 @RequiredArgsConstructor
@@ -28,5 +29,12 @@ public class TagController {
         }
         //TODO: 예외처리
         throw new Exception();
+    }
+
+    @Operation(summary = "최근 가장 많이 사용된 태그 10개 반환", description = "1주일 내 가장 많이 사용된 태그 반환")
+    @GetMapping("")
+    public ResponseEntity<FindTop10TagsResponse> findTop10Tags() {
+        return ResponseEntity.ok()
+                .body(tagService.findTop10Tags());
     }
 }
