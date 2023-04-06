74f1d1d3fad25c56e797b6ac5fea48ef993d7aa4
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-06 17:52:23 +0900

    feat: findOpinions api 생성

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/OpinionController.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/OpinionController.java
@@ -1,15 +1,15 @@
 package com.cocktailmasters.backend.vote.controller;
 
 import com.cocktailmasters.backend.vote.controller.dto.opinion.CreateOpinionRequest;
+import com.cocktailmasters.backend.vote.controller.dto.opinion.FindOpinionsResponse;
 import com.cocktailmasters.backend.vote.service.OpinionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "opinion", description = "의견 관리")
 @RequiredArgsConstructor
@@ -21,9 +21,9 @@ public class OpinionController {
 
     @Operation(summary = "의견 작성", description = "투표 글에 의견 작성")
     @PostMapping()
-    public ResponseEntity<String> createOpinion(@RequestBody CreateOpinionRequest createOpinionRequest) throws Exception {
+    public ResponseEntity<String> createOpinion(@Valid @RequestBody CreateOpinionRequest createOpinionRequest) throws Exception {
         // TODO: 토큰으로 사용자 검증
-        if(createOpinionRequest.getContent().trim().isEmpty()) {
+        if (createOpinionRequest.getContent().trim().isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
         Long userId = 1L;
@@ -32,4 +32,16 @@ public class OpinionController {
         }
         throw new Exception();
     }
+
+    @Operation(summary = "의견 목록 보기",
+            description = "의견 목록을 페이지당 10개씩 반환, 정렬은 1: agree, 2:recent")
+    @GetMapping("/{vote_id}/{page_index}")
+    public ResponseEntity<FindOpinionsResponse> findOpinions(@PathVariable("vote_id") Long voteId,
+                                                             @PathVariable("page_index") int pageIndex,
+                                                             @RequestParam(value = "sort-by", defaultValue = "1") int sortBy) {
+        PageRequest page = PageRequest.of(pageIndex, 10);
+        return ResponseEntity.ok()
+                .body(opinionService.findOpinions(voteId, sortBy, page));
+    }
+
 }
