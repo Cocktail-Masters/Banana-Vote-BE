a0468be29c5702f7da37b5153367eae77291a87c
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-06 17:35:22 +0900

    feat: FindOpinionsResponse dto 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/opinion/FindOpinionsResponse.java
@@ -0,0 +1,16 @@
+package com.cocktailmasters.backend.vote.controller.dto.opinion;
+
+import com.cocktailmasters.backend.vote.controller.dto.item.OpinionDto;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class FindOpinionsResponse {
+
+    List<OpinionDto> opinions;
+    List<Integer> bestIds;
+    int totalCount;
+}
