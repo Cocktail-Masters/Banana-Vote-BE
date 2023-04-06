bfdae32c50b3d4a7f1e83660bcd89a5dfb725801
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-06 17:38:36 +0900

    feat: findOpinionsByVoteIdAndOption 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/vote/domain/repository/OpinionRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/domain/repository/OpinionRepository.java
@@ -1,11 +1,31 @@
 package com.cocktailmasters.backend.vote.domain.repository;
 
 import com.cocktailmasters.backend.vote.domain.entity.Opinion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OpinionRepository extends JpaRepository<Opinion, Long> {
 
-    Optional<Opinion> findFirstByVoteIdOrderByAgreedNumberDesc(Long VoteId);
+    String countOpinionsQuery = "SELECT COUNT(o,*) " +
+            "FROM OPINION o " +
+            "WHERE o.vote_id = :voteId " +
+            "AND is_active = true";
+
+    Optional<Opinion> findFirstByVoteIdOrderByAgreedNumberDesc(Long voteId);
+
+    @Query(value = "SELECT o.* " +
+            "FROM opinion o " +
+            "WHERE o.vote_id = :vote_id " +
+            "AND is_active = true " +
+            "ORDER BY :sort_by",
+            countQuery = "countOpinionsQuery",
+            nativeQuery = true)
+    Page<Opinion> findOpinionsByVoteIdAndOption(@Param("vote_id") Long voteId,
+                                                @Param("sort_by") String sortBy,
+                                                Pageable pageable);
 }
