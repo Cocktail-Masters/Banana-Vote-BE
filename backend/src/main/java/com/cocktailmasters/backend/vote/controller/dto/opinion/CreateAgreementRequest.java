2424b4cfba5df60c976930b49f4cd82950823209
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-11 14:39:27 +0900

    feat: CreateAgreementRequest dto 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/opinion/CreateAgreementRequest.java
@@ -0,0 +1,24 @@
+package com.cocktailmasters.backend.vote.controller.dto.opinion;
+
+import com.cocktailmasters.backend.account.domain.entity.User;
+import com.cocktailmasters.backend.vote.domain.entity.Agreement;
+import com.cocktailmasters.backend.vote.domain.entity.Opinion;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CreateAgreementRequest {
+
+    @NotNull
+    private Boolean isAgree;
+
+    public Agreement toAgreementEntity(User user, Opinion opinion) {
+        return Agreement.builder()
+                .user(user)
+                .opinion(opinion)
+                .isAgree(isAgree)
+                .build();
+    }
+}
