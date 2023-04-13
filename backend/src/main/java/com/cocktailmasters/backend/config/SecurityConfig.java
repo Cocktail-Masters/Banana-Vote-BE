46bf3e84f34fbd8c5dfb673191410e94a55fb482
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-13 17:07:15 +0900

    feat: SecurityConfig 클래스 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.cocktailmasters.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+}
