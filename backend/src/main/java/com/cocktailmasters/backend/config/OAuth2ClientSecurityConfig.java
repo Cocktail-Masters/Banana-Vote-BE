6a3b5e7f0d079cc649c8ea9cf2e1393423829add
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-17 21:16:34 +0900

    feat: JWT 설정

--- backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
@@ -1,19 +1,31 @@
 package com.cocktailmasters.backend.config;
 
+import io.jsonwebtoken.Jwt;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.token.TokenService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class OAuth2ClientSecurityConfig {
 
     private static final String CONTEXT_PATH = "/api/v1/members";
+
+    private final OAuth2ResourceServerConfigurer.JwtConfigurer jwtConfigurer;
+
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public SecurityFilterChain filterChain(HttpSecurity http, OAuth2ResourceServerProperties.Jwt jwt, TokenService tokenService) throws Exception {
         http
