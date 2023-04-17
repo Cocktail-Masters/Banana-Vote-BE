6243431315dacd5c34b9982302583f54be841321
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-17 16:26:35 +0900

    feat: add buy goods API and add quantity param

--- backend/src/main/java/com/cocktailmasters/backend/goods/controller/GoodsController.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/GoodsController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +41,7 @@ public class GoodsController {
     @Operation(summary = "상품들의 리스트 반환",
         description = "type 미 명시 시 전체, 정렬 기준 default는 1 (1, 2 이외의 값이들어갔을 경우 1로 취급), 기본 페이지는 0, 기본 페이지 사이즈는 10")
     @GetMapping("/list")
-    public ResponseEntity<GoodsResponse>getGoods(@RequestParam(required = false, name = "type", defaultValue = "-1") String goodsTypeName,
+    public ResponseEntity<GoodsResponse> getGoods(@RequestParam(required = false, name = "type", defaultValue = "-1") String goodsTypeName,
         @RequestParam(required = false, name = "sortby", defaultValue = "1") int sortBy,
         @RequestParam(required = false, name = "page", defaultValue = "0") int page,
         @RequestParam(required = false, name = "size", defaultValue = "10") int pageSize) {
@@ -53,5 +55,22 @@ public class GoodsController {
             return ResponseEntity.noContent().build();
         else
             return ResponseEntity.ok().body(goodsResponse);
+    }
+
+    @Operation(summary = "상품을 구매",
+        description = "상품의 수량이 부족할 경우, 구매 일자가 유효하지 않을 경우 에러, 상품이 존재하지 않을 경우에도 에러")
+    @PostMapping("/{goodsId}")
+    public ResponseEntity<String> buyGoods(@PathVariable long goodsId,
+        @RequestParam(required = false, name = "quantity", defaultValue = "1") long quantity) {
+        // TODO : 로그인 여부 확인 및 유저 아이디 확인
+        long userId = 0;
+
+        if(quantity < 0)
+            return ResponseEntity.badRequest().build();
+
+        if(!goodsService.buyGoods(userId, quantity, goodsId))
+            return ResponseEntity.notFound().build();
+        else            
+            return ResponseEntity.ok().build();
     }
 }
