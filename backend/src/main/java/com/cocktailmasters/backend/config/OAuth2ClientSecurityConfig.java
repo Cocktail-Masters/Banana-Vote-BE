c4f4d3e5a813027d8566f685af19200ce8471398
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-18 17:25:07 +0900

    feat: clear git cache

--- backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
@@ -1,16 +1,14 @@
 package com.cocktailmasters.backend.config;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
-@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
+@Configuration
 public class OAuth2ClientSecurityConfig {
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
