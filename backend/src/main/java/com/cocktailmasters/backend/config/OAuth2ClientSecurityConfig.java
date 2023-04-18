7910966a6a2cd4d03f7bf9584ddf46f0098dae8e
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-18 16:06:30 +0900

    feat: 카카오 로그인 API를 위한 시큐리티 설정

--- backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
@@ -1,37 +1,29 @@
 package com.cocktailmasters.backend.config;
 
-import io.jsonwebtoken.Jwt;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.token.TokenService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class OAuth2ClientSecurityConfig {
 
-    private static final String CONTEXT_PATH = "/api/v1/members";
-
-    private final OAuth2ResourceServerConfigurer.JwtConfigurer jwtConfigurer;
-
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public SecurityFilterChain filterChain(HttpSecurity http, OAuth2ResourceServerProperties.Jwt jwt, TokenService tokenService) throws Exception {
-        http
-                .authorizeRequests(authorize -> authorize.anyRequest()
-                        .authenticated())
-                .oauth2Login(Customizer.withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(null);
         return http.build();
     }
 }
