3742a2a847af83331da018272090432dd26d705d
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-20 15:55:52 +0900

    feat: KakaoLoginParams class 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/authentication/infra/kakao/KakaoInfoReponse.java
@@ -0,0 +1,57 @@
+package com.cocktailmasters.backend.authentication.infra.kakao;
+
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthInfoResponse;
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthProvider;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoInfoReponse implements OAuthInfoResponse {
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoAccount {
+
+        private KakaoProfile profile;
+        private String email;
+        private String ageRange;
+        private String gender;
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoProfile {
+
+        private String nickname;
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccount.email;
+    }
+
+    @Override
+    public String getNickname() {
+        return kakaoAccount.profile.nickname;
+    }
+
+    @Override
+    public String getGender() {
+        return kakaoAccount.gender;
+    }
+
+    @Override
+    public String getAgeRange() {
+        return kakaoAccount.ageRange;
+    }
+
+    @Override
+    public OAuthProvider getOAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+}
---
+++ backend/src/main/java/com/cocktailmasters/backend/authentication/infra/kakao/KakaoLoginParams.java
@@ -0,0 +1,27 @@
+package com.cocktailmasters.backend.authentication.infra.kakao;
+
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthLoginParams;
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthProvider;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
