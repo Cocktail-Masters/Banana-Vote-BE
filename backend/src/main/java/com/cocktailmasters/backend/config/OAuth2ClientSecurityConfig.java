7241974861bbad188b1e33261a371e0e5af60b7c
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-22 20:28:52 +0900

    feat: RestTemplate 생성

--- backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/OAuth2ClientSecurityConfig.java
@@ -1,27 +1,33 @@
 package com.cocktailmasters.backend.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientSecurityConfig {
 
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(null);
-        return http.build();
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .cors().disable()
+//                .authorizeRequests(request -> request
+//                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+//                        .requestMatchers("").permitAll()
+//                        .anyRequest().authenticated())
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .and()
+//                .oauth2Login()
+//                .userInfoEndpoint()
+//                .userService(null);
+//        return http.build();
+//    }
 }
