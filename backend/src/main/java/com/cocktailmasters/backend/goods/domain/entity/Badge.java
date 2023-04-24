456aa99a5fc82b223e3b46d25ad84a435b923679
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-24 17:36:40 +0900

    feat: add modifiy entity function with dto

--- backend/src/main/java/com/cocktailmasters/backend/goods/domain/entity/Badge.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/domain/entity/Badge.java
@@ -1,6 +1,8 @@
 package com.cocktailmasters.backend.goods.domain.entity;
 
 import com.cocktailmasters.backend.common.domain.entity.BaseEntity;
+import com.cocktailmasters.backend.goods.controller.dto.BadgeRequest;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -38,4 +40,13 @@ public class Badge extends BaseEntity {
     @Builder.Default
     @OneToMany(mappedBy = "badge", cascade = CascadeType.ALL)
     private List<UserBadge> userBadges = new ArrayList<>();
+
+    public void modifiyBadgeWithDto(BadgeRequest badge) {
+        this.badgeName = badge.getName();
+        this.badgeImageUrl = badge.getImageUrl();
+        this.badgeDescription = badge.getDescription();
+        this.badgePrice = badge.getPrice();
+        this.isSelling = badge.isSelling();
+        this.badgeEndDate = badge.getBadgeEndDate();
+    }
 }
