5297f52ca0d6b3be5812c611a4406700defa0ea2
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-24 22:49:20 +0900

    feat: RequestIOAuthInfoService 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/authentication/domain/oauth/RequestIOAuthInfoService.java
@@ -0,0 +1,25 @@
+package com.cocktailmasters.backend.authentication.domain.oauth;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+public class RequestIOAuthInfoService {
+    private final Map<OAuthProvider, OAuthApiClient> clients;
+
+    public RequestIOAuthInfoService(List<OAuthApiClient> clients) {
+        this.clients = clients.stream().collect(
+                Collectors.toUnmodifiableMap(OAuthApiClient::oAuthProvider, Function.identity())
+        );
+    }
+
+    public OAuthInfoResponse request(OAuthLoginParams params) {
+        OAuthApiClient client = clients.get(params.oAuthProvider());
+        String accessToken = client.requestAccessToken(params);
+        return client.requestOauthInfo(accessToken);
+    }
+}
