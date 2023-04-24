2d2651b0af3e6480f9ac6694662a2e64f49de1a0
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-24 15:55:54 +0900

    feat: add badge Request dto

---
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/dto/BadgeRequest.java
@@ -0,0 +1,39 @@
+package com.cocktailmasters.backend.goods.controller.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BadgeRequest {
+    
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @JsonProperty("image_url")
+    private String imageUrl;
+
+    @NotNull
+    private String description;
+
+    @Builder.Default
+    @JsonProperty("is_selling")
+    private boolean isSelling = true;
+
+    @JsonProperty("end_date")
+    private LocalDate badgeEndDate;
+
+    @Builder.Default
+    private long price = 0L;
+}
