1b373ed27df3e58b5ad6ebd860cf65d8aa30aa7c
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-26 15:08:22 +0900

    feat: add get list of badeges in respogitory

--- backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/BadgeRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/BadgeRepository.java
@@ -1,7 +1,13 @@
 package com.cocktailmasters.backend.goods.domain.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cocktailmasters.backend.goods.domain.entity.Badge;
 
-public interface BadgeRepository extends JpaRepository<Badge, Long> { }
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
+    List<Badge> findAllByBadgeEndDateBeforeAndIsSelling(LocalDate date, boolean isSelling);
+    List<Badge> findAllByIsSelling(boolean isSelling);
+}
