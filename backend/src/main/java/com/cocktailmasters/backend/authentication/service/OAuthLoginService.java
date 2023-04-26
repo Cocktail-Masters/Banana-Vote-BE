3a8b6fd07ba670726d283995a89279e087801b98
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-26 17:24:26 +0900

    feat: login, findOrCreateMember, newUser 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/authentication/service/OAuthLoginService.java
+++ backend/src/main/java/com/cocktailmasters/backend/authentication/service/OAuthLoginService.java
@@ -1,7 +1,12 @@
 package com.cocktailmasters.backend.authentication.service;
 
+import com.cocktailmasters.backend.account.domain.entity.User;
 import com.cocktailmasters.backend.account.domain.repository.UserRepository;
+import com.cocktailmasters.backend.authentication.domain.AuthTokens;
 import com.cocktailmasters.backend.authentication.domain.AuthTokensGenerator;
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthInfoResponse;
+import com.cocktailmasters.backend.authentication.domain.oauth.OAuthLoginParams;
+import com.cocktailmasters.backend.authentication.domain.oauth.RequestOAuthInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +16,28 @@ public class OAuthLoginService {
 
     private final UserRepository userRepository;
     private final AuthTokensGenerator authTokensGenerator;
+    private final RequestOAuthInfoService requestOAuthInfoService;
+
+    public AuthTokens login(OAuthLoginParams params) {
+        OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
+        Long memberId = findOrCreateMember(oAuthInfoResponse);
+        return authTokensGenerator.generate(memberId);
+    }
+
+    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
+        return userRepository.findByEmail(oAuthInfoResponse.getEmail())
+                .map(User::getId)
+                .orElseGet(() -> newUser(oAuthInfoResponse));
+    }
+
+    private Long newUser(OAuthInfoResponse oAuthInfoResponse) {
+        User user = User.builder()
+                .email(oAuthInfoResponse.getEmail())
+                .email(oAuthInfoResponse.getEmail())
+                .nickname(oAuthInfoResponse.getNickname())
+                .gender(oAuthInfoResponse.getGender())
+                .oAuthProvider(oAuthInfoResponse.getOAuthProvider())
+                .build();
+        return userRepository.save(user).getId();
+    }
 }
