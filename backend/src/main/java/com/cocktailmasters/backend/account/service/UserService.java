0fc6decbd4c215c89923b3546ab850aaab2dd041
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-27 23:32:27 +0900

    feat: signUp 함수 생성

--- backend/src/main/java/com/cocktailmasters/backend/account/service/UserService.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/service/UserService.java
@@ -1,10 +1,28 @@
 package com.cocktailmasters.backend.account.service;
 
+import com.cocktailmasters.backend.account.controller.dto.UserSignUpRequest;
+import com.cocktailmasters.backend.account.domain.entity.User;
+import com.cocktailmasters.backend.account.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(UserSignUpRequest userSignUpRequest) throws Exception {
+        if (userRepository.findByEmail(userSignUpRequest.getEmail()).isPresent()) {
+            throw new Exception();
+        }
+        if (userRepository.findByNickname(userSignUpRequest.getEmail()).isPresent()) {
+            throw new Exception();
+        }
+        User user = userSignUpRequest.toUserEntity();
+        user.encodePassword(passwordEncoder);
+        userRepository.save(user);
+    }
 }
