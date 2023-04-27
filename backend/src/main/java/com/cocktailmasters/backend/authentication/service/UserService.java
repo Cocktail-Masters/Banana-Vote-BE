3c5e36dc64d71aad8491664ef2fd2ff8ac1d3b11
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-27 17:28:21 +0900

    feat: UserService 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/authentication/service/UserService.java
@@ -0,0 +1,14 @@
+package com.cocktailmasters.backend.authentication.service;
+
+import com.cocktailmasters.backend.account.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+}
