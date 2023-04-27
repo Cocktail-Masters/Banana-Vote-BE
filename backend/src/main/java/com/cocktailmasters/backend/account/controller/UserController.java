0499f45cb11768cb3e2cfd160af1f494a0a95efa
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-27 23:49:24 +0900

    feat: signUp 및 jwtTest api 생성

--- backend/src/main/java/com/cocktailmasters/backend/account/controller/UserController.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/controller/UserController.java
@@ -1,7 +1,33 @@
 package com.cocktailmasters.backend.account.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.cocktailmasters.backend.account.controller.dto.SignUpRequest;
+import com.cocktailmasters.backend.account.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "user", description = "회원 관리")
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "일반 회원 가입", description = "일반 회원 가입")
+    @PostMapping("/sign-up")
+    public ResponseEntity<String> signUp(@RequestBody @Valid SignUpRequest signUpRequest) throws Exception {
+        if (userService.signUp(signUpRequest)) {
+            return ResponseEntity.created(null).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
+    }
 }
