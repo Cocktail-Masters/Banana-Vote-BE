320bf629d62594ec79f7a9489a1cffc19305ace2
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-28 00:57:03 +0900

    feat: JwtService 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/jwt/service/JwtService.java
@@ -0,0 +1,9 @@
+package com.cocktailmasters.backend.jwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtService {
+}
