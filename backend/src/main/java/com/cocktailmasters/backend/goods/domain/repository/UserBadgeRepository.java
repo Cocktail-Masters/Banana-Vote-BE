023f6a4a9082e529c639ce26ff4afa2c0f00f849
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-04-27 18:11:39 +0900

    feat: add query to repository for getting user badges

--- backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/UserBadgeRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/UserBadgeRepository.java
@@ -1,7 +1,11 @@
 package com.cocktailmasters.backend.goods.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cocktailmasters.backend.goods.domain.entity.UserBadge;
 
-public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> { }
+public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
+    List<UserBadge> findAllByUserId(long userId);
+}
