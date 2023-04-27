7b89a45bf0b3379f3d20458f1e3a77bcde2d8be3
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-27 17:40:54 +0900

    feat: passwordEncoder 추가

--- backend/src/main/java/com/cocktailmasters/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/SecurityConfig.java
@@ -1,29 +1,39 @@
 package com.cocktailmasters.backend.config;
 
-import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .cors().disable()
-                .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(login -> login
-                        .defaultSuccessUrl("/home", true)
-                        .permitAll()
-                )
-                .logout(withDefaults());
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/login", "/sign-up").permitAll()
+                .anyRequest().authenticated();
+//                .and()
+//                .oauth2Login()
+//                .successHandler(oAuth2LoginSuccessHandler)
+//                .failureHandler(oAuth2LoginFailureHandler)
+//                .userInfoEndpoint().userService();
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
