3be5d735653fd769a3cc9cb23332a30736852e77
swoody1101@gmail.com <swoody1101@gmail.com>
2023-04-29 23:25:41 +0900

    feat: 디렉토리 변경

--- backend/src/main/java/com/cocktailmasters/backend/account/jwt/JwtProvider.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/jwt/JwtProvider.java
@@ -1,4 +1,4 @@
-package com.cocktailmasters.backend.account.jwt.infra;
+package com.cocktailmasters.backend.account.jwt;
 
 import com.cocktailmasters.backend.account.user.domain.entity.User;
 import io.jsonwebtoken.*;
@@ -75,8 +75,20 @@ public class JwtProvider {
         } catch (IllegalArgumentException e) {
             log.error("JWT token is invalid");
             return false;
+        } catch ( ExpiredJwtException e) {
+
+            return false;
         }
     }
+
+    public void sendAccessToken() {
+    }
+
+    public void sendAccessAndRefreshToken() {
+
+    }
+    //TODO: 토큰 헤더에 추가 후 보내기
+    //TODO: 토큰 값 가져오기
 
     private String createToken(String tokenType, User user, Long tokenExpirationDate) {
         Claims claims = Jwts.claims();
@@ -91,14 +103,14 @@ public class JwtProvider {
                 .compact();
     }
 
+    private Date createExpireDate(Long tokenExpirationDate) {
+        Date now = new Date();
+        return new Date(now.getTime() + tokenExpirationDate);
+    }
+
     private Optional<String> extractToken(HttpServletRequest request, String tokenHeader) {
         return Optional.ofNullable(request.getHeader(tokenHeader))
                 .filter(token -> token.startsWith(BEARER))
                 .map(token -> refreshTokenHeader.replace(BEARER, ""));
-    }
-
-    private Date createExpireDate(Long tokenExpirationDate) {
-        Date now = new Date();
-        return new Date(now.getTime() + tokenExpirationDate);
     }
 }
