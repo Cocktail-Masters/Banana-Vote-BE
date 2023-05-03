a3437dbdf26e34d69a57d16163c8a582f5c22729
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-05-03 16:42:43 +0900

    feat: find by badge id and user id

--- backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/UserBadgeRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/UserBadgeRepository.java
@@ -1,6 +1,7 @@
 package com.cocktailmasters.backend.goods.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import com.cocktailmasters.backend.goods.domain.entity.UserBadge;
 
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
     List<UserBadge> findAllByUserId(long userId);
+
+    Optional<UserBadge> findByBadgeIdAndUserId(long BadgeId, long UserId);
 }
