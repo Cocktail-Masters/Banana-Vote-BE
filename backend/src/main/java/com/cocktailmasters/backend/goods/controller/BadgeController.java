2b210a4ae08ff8e861694e889ebbf459ff02fc8d
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-05-03 16:41:31 +0900

    feat: add badge admin api logic

--- backend/src/main/java/com/cocktailmasters/backend/goods/controller/BadgeController.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/BadgeController.java
@@ -2,6 +2,7 @@ package com.cocktailmasters.backend.goods.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cocktailmasters.backend.goods.controller.dto.BadgeRequest;
 import com.cocktailmasters.backend.goods.controller.dto.BadgeResponse;
+import com.cocktailmasters.backend.goods.controller.dto.UserBadgeResponse;
 import com.cocktailmasters.backend.goods.service.BadgesService;
+import com.cocktailmasters.backend.goods.service.UserBadgeService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,6 +32,7 @@ import lombok.RequiredArgsConstructor;
 public class BadgeController {
     
     private final BadgesService badgeService;
+    private final UserBadgeService userBadgeService;
 
     @Operation(summary = "뱃지 생성(관리자용)", description = "뱃지를 생성")
     @PostMapping
@@ -75,6 +79,47 @@ public class BadgeController {
         if(badges.size() == 0)
             return ResponseEntity.noContent().build();
         else
-            return ResponseEntity.ok(badges);
+            return ResponseEntity.ok().body(badges);
+    }
+
+    @Operation(summary = "유저가 소유 중인 뱃지 리스트 반환", description = "유저가 소유 중인 뱃지 리스트 반환 (로그인 필요)")
+    @GetMapping("/users")
+    public ResponseEntity<List<UserBadgeResponse>> getUsersBadgeList() {
+        // TODO : check login validation logic
+        long userId = 1L;
+
+        List<UserBadgeResponse> badgeResponses = userBadgeService.getUserBadgesList(userId);
+
+        if(badgeResponses.size() == 0)
+            return ResponseEntity.noContent().build();
+        else
+            return ResponseEntity.ok().body(badgeResponses);
+    }
+
+    @Operation(summary = "유저에게 해당 뱃지를 추가(관리자용)", description = "해당 유저에게 뱃지를 추가, 유저가 없거나 추가할 뱃지가 없거나, \n해당 유저가 이미 그 뱃지를 가지고 있을 경우 X")
+    @PostMapping("/users/{badgeId}")
+    public ResponseEntity<String> addBadgeToUser(@PathVariable long badgeId,
+                                                @RequestParam(required = true, name = "userId") long userId) {
+        // TODO : 관리자 검증
+        int res = userBadgeService.addBadgeToUser(badgeId, userId);
+
+        if(res == 1)
+            return ResponseEntity.created(null).build();                  
+        else if(res == 0)
+            return ResponseEntity.notFound().build();                                
+        else
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @Operation(summary = "유저에게 해당 뱃지를 추가(관리자용)", description = "해당 유저에게 뱃지를 추가, 유저가 없거나 추가할 뱃지가 없거나, \n해당 유저가 이미 그 뱃지를 가지고 있지 않을 경우X")
+    @DeleteMapping("/users/{badgeId}")
+    public ResponseEntity<String> deleteBadgeToUser(@PathVariable long badgeId, 
+    @RequestParam(required = true, name = "userId") long userId) {
+        // TODO : 관리자 검증
+
+        if(userBadgeService.deleteBadgeFromUser(badgeId, userId))
+            return ResponseEntity.ok().build();
+        else
+            return ResponseEntity.notFound().build();
     }
 }
