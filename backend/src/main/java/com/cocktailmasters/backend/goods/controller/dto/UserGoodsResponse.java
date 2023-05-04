48572a5b376cda23e5d6844832efb23892a7c02c
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-05-04 15:20:32 +0900

    feat: add user goods response dto

---
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/dto/UserGoodsResponse.java
@@ -0,0 +1,41 @@
+package com.cocktailmasters.backend.goods.controller.dto;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.cocktailmasters.backend.goods.domain.GoodsType;
+import com.cocktailmasters.backend.goods.domain.entity.UserGoods;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserGoodsResponse {
+
+    private long id;
+    private String name;
+    private String description;
+
+    @JsonProperty("image_url")
+    private String imageUrl;
+
+    private GoodsType type;
+    private long ea;
+
+    @JsonProperty("expiration_date")
+    private String expirationDate;
+
+    public static UserGoodsResponse createUserGoodsReponse(UserGoods userGoods) {
+        return UserGoodsResponse.builder()
+                    .id(userGoods.getId())
+                    .name(userGoods.getGoods().getGoodsName())
+                    .description(userGoods.getGoods().getGoodsDescription())
+                    .imageUrl(userGoods.getGoods().getGoodsImageUrl())
+                    .type(userGoods.getGoods().getGoodsType())
+                    .ea(userGoods.getGoodsAmount())
+                    .expirationDate(userGoods.getGoodsExpirationDate().format(DateTimeFormatter.ISO_LOCAL_DATE))
+                    .build();
+    }
+}
