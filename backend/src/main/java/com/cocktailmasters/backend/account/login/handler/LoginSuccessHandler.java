86220e5e07e69f68dd6faef893d2f0ae33bfb821
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-09 23:53:15 +0900

    feat: LoginSuccessHandler class 생성

---
+++ backend/src/main/java/com/cocktailmasters/backend/account/login/handler/LoginSuccessHandler.java
@@ -0,0 +1,54 @@
+package com.cocktailmasters.backend.account.login.handler;
+
+import com.cocktailmasters.backend.account.jwt.service.JwtService;
+import com.cocktailmasters.backend.account.user.domain.entity.User;
+import com.cocktailmasters.backend.account.user.domain.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@PropertySource("application-dev.yml")
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Value("${jwt.access-token.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String email = extractUsername(authentication);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email does not exist"));
+        String accessToken = jwtService.createAccessToken(user);
+        String refreshToken = jwtService.createRefreshToken(user);
+        jwtService.setAccessTokenHeader(response, accessToken);
+        jwtService.setRefreshTokenHeader(response, refreshToken);
+        user.updateRefreshToken(refreshToken);
+        userRepository.saveAndFlush(user);
+        log.info("로그인에 성공하였습니다. 이메일 : {}", email);
+        log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
+        log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
+        super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private String extractUsername(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+}
