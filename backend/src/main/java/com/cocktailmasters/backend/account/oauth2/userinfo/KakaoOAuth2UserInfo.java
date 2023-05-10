ac0a864db4a21f2a61b67adc8f04de8624f2014a
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-11 00:56:48 +0900

    feat: create KakaoOAuth2UserInfo class

---
+++ backend/src/main/java/com/cocktailmasters/backend/account/oauth2/userinfo/KakaoOAuth2UserInfo.java
@@ -0,0 +1,44 @@
+package com.cocktailmasters.backend.account.oauth2.userinfo;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+
+    @Override
+    public String getId() {
+        return (String) String.valueOf(attributes.get("id"));
+    }
+
+    @Override
+    public String getNickname() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+        if (account.isEmpty() || profile.isEmpty()) {
+            return null;
+        }
+        return (String) profile.get("nickname");
+    }
+
+    @Override
+    public String getGender() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        if (account.isEmpty()) {
+            return null;
+        }
+        return (String) account.get("gender");
+    }
+
+    @Override
+    public String getAge() {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        if (account.isEmpty()) {
+            return null;
+        }
+        return (String) account.get("age_range");
+    }
+}
