1b6f19b8ce20767a19263d49186c8654deb0b7c2
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-05-10 15:45:07 +0900

    feat: add delete modifiy API imple, and fix style

--- backend/src/main/java/com/cocktailmasters/backend/goods/service/GoodsService.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/service/GoodsService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cocktailmasters.backend.goods.controller.dto.GoodsRequest;
 import com.cocktailmasters.backend.goods.controller.dto.GoodsResponse;
 import com.cocktailmasters.backend.goods.controller.dto.item.GoodsItemDto;
 import com.cocktailmasters.backend.goods.domain.GoodsType;
@@ -25,7 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class GoodsService {
-    
+
     private final GoodsRepository goodsRepository;
 
     private final PointService pointService;
@@ -33,6 +34,7 @@ public class GoodsService {
 
     /**
      * get all types
+     * 
      * @return enum List
      */
     public List<GoodsType> getGoodsTypes() {
@@ -41,12 +43,13 @@ public class GoodsService {
 
     /**
      * return GoodsType enum by String
+     * 
      * @param goodsType
      * @return correct goodstype or null if not exists
      */
     public GoodsType getGoodsTypeByName(String goodsType) {
-        for(GoodsType key : GoodsType.values()) {
-            if(key.name().equals(goodsType.toUpperCase()))
+        for (GoodsType key : GoodsType.values()) {
+            if (key.name().equals(goodsType.toUpperCase()))
                 return key;
         }
         return null;
@@ -54,8 +57,9 @@ public class GoodsService {
 
     /**
      * find by type or all and return Goods List Response by paging
-     * @param type if null, find all
-     * @param sortBy 2 : sold count, 1 : recently
+     * 
+     * @param type     if null, find all
+     * @param sortBy   2 : sold count, 1 : recently
      * @param page
      * @param pageSize
      * @return GoodsResponse
@@ -64,13 +68,13 @@ public class GoodsService {
         Pageable pageable = PageRequest.of(page, pageSize);
 
         List<Goods> goodsList;
-        if(type == null) { // find all
-            if(sortBy == 2) // popular first
+        if (type == null) { // find all
+            if (sortBy == 2) // popular first
                 goodsList = goodsRepository.findAllByOrderByGoodsSoldNumberAsc(pageable).getContent();
             else // newly first
                 goodsList = goodsRepository.findAllByOrderBySaleStartDateAsc(pageable).getContent();
         } else { // find by type
-            if(sortBy == 2) // popular first
+            if (sortBy == 2) // popular first
                 goodsList = goodsRepository.findByGoodsTypeOrderByGoodsSoldNumberAsc(type, pageable).getContent();
             else // newly first
                 goodsList = goodsRepository.findByGoodsTypeOrderBySaleStartDateAsc(type, pageable).getContent();
@@ -78,7 +82,7 @@ public class GoodsService {
 
         // change to dto
         List<GoodsItemDto> goodsDtosList = new ArrayList<>();
-        for(int i = 0; i < goodsList.size(); i++)
+        for (int i = 0; i < goodsList.size(); i++)
             goodsDtosList.add(GoodsItemDto.createGoodItemDto(goodsList.get(i)));
 
         // get total page
@@ -90,13 +94,14 @@ public class GoodsService {
     /**
      * count total page size by type
      * if type is null. it count by all
+     * 
      * @param type
      * @param pageSize
      * @return total page size
      */
     public int getGoodsListTotalPages(GoodsType type, int pageSize) {
         long totalCount;
-        if(type == null)
+        if (type == null)
             totalCount = goodsRepository.count();
         else
             totalCount = goodsRepository.countByGoodsType(type);
@@ -106,6 +111,7 @@ public class GoodsService {
 
     /**
      * buy goods and apply quantity with point
+     * 
      * @param userId
      * @param quantity
      * @param goodsId
@@ -115,29 +121,89 @@ public class GoodsService {
     @Transactional
     public boolean buyGoods(long userId, int quantity, long goodsId) throws CustomException {
         Optional<Goods> goods = goodsRepository.findById(goodsId);
-        if(!goods.isPresent()) return false;
+        if (!goods.isPresent())
+            return false;
 
         // check selling date
         LocalDate currentDate = LocalDate.now();
-        if(currentDate.isAfter(goods.get().getSaleEndDate()))
+        if (currentDate.isAfter(goods.get().getSaleEndDate()))
             throw new CustomException(HttpStatus.CONFLICT, "out of date");
         // check quantity
-        if(goods.get().getGoodsRemainingQuantity() >= 0 && goods.get().getGoodsRemainingQuantity() - quantity < 0)
+        if (goods.get().getGoodsRemainingQuantity() >= 0 && goods.get().getGoodsRemainingQuantity() - quantity < 0)
             throw new CustomException(HttpStatus.CONFLICT, "lack of quantity");
 
         // apply point
         String logMessage = goods.get().getGoodsName() + " 구매";
-        if(!pointService.addPoint(goods.get().getGoodsPrice() * -1 * quantity, logMessage, userId))
+        if (!pointService.addPoint(goods.get().getGoodsPrice() * -1 * quantity, logMessage, userId))
             throw new CustomException(HttpStatus.CONFLICT, "lack of point");
-        
+
         // apply quanity and sold count
         goods.get().soldGoods(quantity);
         goodsRepository.save(goods.get());
 
         // add goods to user
-        if(!userGoodsService.addUserGoodsToUser(userId, quantity, goodsId))
-            throw new CustomException(HttpStatus.NOT_FOUND, "user or goods not found");    
-        
+        if (!userGoodsService.addUserGoodsToUser(userId, quantity, goodsId))
+            throw new CustomException(HttpStatus.NOT_FOUND, "user or goods not found");
+
+        return true;
+    }
+
+    /**
+     * add goods (for admin)
+     * 
+     * @param goodsRequest
+     * @return true
+     */
+    @Transactional
+    public boolean addGoods(GoodsRequest goodsRequest) {
+        Goods addedGoods = Goods.builder()
+                .goodsName(goodsRequest.getName())
+                .goodsDescription(goodsRequest.getDescription())
+                .goodsType(goodsRequest.getType())
+                .goodsImageUrl(goodsRequest.getImageUrl())
+                .goodsValidityPeriod(goodsRequest.getValidPeriod())
+                .goodsPrice(goodsRequest.getPrice())
+                .goodsRemainingQuantity(goodsRequest.getRemainingQuantity())
+                .saleStartDate(goodsRequest.getStartDate())
+                .saleEndDate(goodsRequest.getStartDate())
+                .build();
+
+        goodsRepository.save(addedGoods);
+        return true;
+    }
+
+    /**
+     * delete goods (for admin)
+     * 
+     * @param goodsId
+     * @param goodsRequest
+     * @return true or false(goods not existed)
+     */
+    @Transactional
+    public boolean deleteGoods(long goodsId) {
+        Optional<Goods> goods = goodsRepository.findById(goodsId);
+        if (!goods.isPresent())
+            return false;
+
+        goodsRepository.delete(goods.get());
+        return true;
+    }
+
+    /**
+     * modify goods info (for admin)
+     * 
+     * @param goodsId
+     * @param goodsRequest
+     * @return true or false(goods not found)
+     */
+    @Transactional
+    public boolean modifyGoods(long goodsId, GoodsRequest goodsRequest) {
+        Optional<Goods> goods = goodsRepository.findById(goodsId);
+        if (!goods.isPresent())
+            return false;
+
+        goods.get().modifiyGoodsInfo(goodsRequest);
+        goodsRepository.save(goods.get());
         return true;
     }
 }
