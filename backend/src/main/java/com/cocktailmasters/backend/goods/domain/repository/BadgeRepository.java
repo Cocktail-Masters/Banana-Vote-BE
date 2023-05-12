89c03992c579e3257a90cbf379b983616568dda3
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-05-12 15:41:18 +0900

    fix: repository typo

--- backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/BadgeRepository.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/domain/repository/BadgeRepository.java
@@ -10,25 +10,25 @@ import com.cocktailmasters.backend.goods.domain.entity.Badge;
 
 public interface BadgeRepository extends JpaRepository<Badge, Long> {
 
-    Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgeEndDateAsc(LocalDate date, boolean isSelling,
-            Pageable pageable);
+        Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgeEndDateAsc(LocalDate date, boolean isSelling,
+                        Pageable pageable);
 
-    Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgeSoldNumberAsc(LocalDate date, boolean isSelling,
-            Pageable pageable);
+        Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgeSoldNumberAsc(LocalDate date, boolean isSelling,
+                        Pageable pageable);
 
-    Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgePriceAsc(LocalDate date, boolean isSelling,
-            Pageable pageable);
+        Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgePriceAsc(LocalDate date, boolean isSelling,
+                        Pageable pageable);
 
-    Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgePriceDec(LocalDate date, boolean isSelling,
-            Pageable pageable);
+        Page<Badge> findByBadgeEndDateAfterAndIsSellingOrderByBadgePriceDesc(LocalDate date, boolean isSelling,
+                        Pageable pageable);
 
-    Page<Badge> findAllByOrderByBadgeEndDateAsc(Pageable pageable);
+        Page<Badge> findAllByOrderByBadgeEndDateAsc(Pageable pageable);
 
-    Page<Badge> findAllByOrderByBadgeSoldNumberAsc(Pageable pageable);
+        Page<Badge> findAllByOrderByBadgeSoldNumberAsc(Pageable pageable);
 
-    Page<Badge> findAllByOrderByBadgePriceAsc(Pageable pageable);
+        Page<Badge> findAllByOrderByBadgePriceAsc(Pageable pageable);
 
-    Page<Badge> findAllByOrderByBadgePriceDec(Pageable pageable);
+        Page<Badge> findAllByOrderByBadgePriceDesc(Pageable pageable);
 
-    long countByBadgeEndDateAfterAndIsSelling(LocalDate date, boolean isSelling);
+        long countByBadgeEndDateAfterAndIsSelling(LocalDate date, boolean isSelling);
 }
