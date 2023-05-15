f0ee8e51759994cef5598924adf9a5bc10f7a93d
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-15 16:16:22 +0900

    feat: create findUserByToken method

--- backend/src/main/java/com/cocktailmasters/backend/account/jwt/service/JwtService.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/jwt/service/JwtService.java
@@ -1,7 +1,9 @@
 package com.cocktailmasters.backend.account.jwt.service;
 
 import com.cocktailmasters.backend.account.user.domain.entity.User;
+import com.cocktailmasters.backend.account.user.domain.repository.UserRepository;
 import com.cocktailmasters.backend.util.exception.AuthException;
+import com.cocktailmasters.backend.util.exception.NotFoundUserException;
 import io.jsonwebtoken.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -43,6 +45,8 @@ public class JwtService {
 
     @Value("${jwt.refresh-token.header}")
     private String refreshTokenHeader;
+
+    private final UserRepository userRepository;
 
     public String createAccessToken(User user) {
         return createToken(ACCESS_TOKEN_SUBJECT, user, accessTokenExpirationDate);
@@ -101,6 +105,11 @@ public class JwtService {
         }
     }
 
+    public User findUserByToken(String token) {
+        return userRepository.findByEmail((String) getPayload(token.replace(BEARER, "")).get(EMAIL_CLAIM))
+                .orElseThrow(() -> new NotFoundUserException());
+    }
+
     private String createToken(String tokenType, User user, Long tokenExpirationDate) {
         Claims claims = Jwts.claims();
         claims.put(ID_CLAIM, user.getId());
@@ -122,7 +131,7 @@ public class JwtService {
     private Optional<String> extractToken(HttpServletRequest request, String tokenHeader) {
         return Optional.ofNullable(request.getHeader(tokenHeader))
                 .filter(token -> token.startsWith(BEARER))
-                .map(token -> refreshTokenHeader.replace(BEARER, ""));
+                .map(token -> token.replace(BEARER, ""));
     }
 
     private Claims extractBody(String token) {
