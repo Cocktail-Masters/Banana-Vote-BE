af5f9c49f05ba9669d3573571b8d380de71f4911
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-15 16:24:08 +0900

    feat: add jwtTest example

--- backend/src/main/java/com/cocktailmasters/backend/account/user/controller/UserController.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cocktailmasters.backend.account.user.controller;
 
+import com.cocktailmasters.backend.account.jwt.service.JwtService;
 import com.cocktailmasters.backend.account.user.controller.dto.SignUpRequest;
+import com.cocktailmasters.backend.account.user.domain.entity.User;
 import com.cocktailmasters.backend.account.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 @Slf4j
 @Tag(name = "user", description = "회원 관리")
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
+    private final JwtService jwtService;
     private final UserService userService;
 
     @Operation(summary = "일반 회원 가입", description = "일반 회원 가입")
@@ -30,7 +35,8 @@ public class UserController {
     }
 
     @GetMapping("/jwt-test")
-    public String jwtTest() {
+    public String jwtTest(@RequestHeader(AUTHORIZATION) String token) {
+        User user = jwtService.findUserByToken(token);
         return "jwtTest 요청 성공";
     }
 }
