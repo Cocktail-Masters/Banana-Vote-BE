ad1cc361219e55fa31232dcc93006c4d82ae1aa8
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-23 16:39:11 +0900

    feat: add exposed headers

--- backend/src/main/java/com/cocktailmasters/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/cocktailmasters/backend/config/WebMvcConfig.java
@@ -1,16 +1,24 @@
-package com.cocktailmasters.backend;
+package com.cocktailmasters.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    
+
+    @Value("${jwt.access-token.header}")
+    private String accessTokenHeader;
+
+    @Value("${jwt.refresh-token.header}")
+    private String refreshTokenHeader;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .exposedHeaders(accessTokenHeader, refreshTokenHeader)
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH");
     }
 }
