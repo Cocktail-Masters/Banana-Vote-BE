29c807ef48d2b627cc37916456f2ddfd14babdaf
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-24 17:00:47 +0900

    feat: add makeRedirectUrlWithAccess and makeRedirectUrlWithAccessAndRefresh methods, add redirect method

--- backend/src/main/java/com/cocktailmasters/backend/account/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -10,60 +10,79 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Value("${front.redirect-url}")
+    private final String frontRedirectUri;
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
-            HttpServletResponse response,
-            Authentication authentication) throws IOException {
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
         log.info("Success OAuth2 Login");
+        String redirectUrl;
         try {
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
             User user = findUserByEmail(oAuth2User.getEmail());
             if (oAuth2User.getRole() == Role.GUEST) {
                 String accessToken = jwtService.createAccessToken(user);
+
                 log.info("GUEST access token : " + accessToken);
 
-                response.addHeader(jwtService.getAccessTokenHeader(), "Bearer " + accessToken);
-                // response.sendRedirect("/"); // 프론트의 회원가입 추가 정보 입력 폼 리다이렉트
-                response.setStatus(HttpServletResponse.SC_OK);
                 jwtService.setAccessTokenHeader(response, accessToken);
+                redirectUrl = makeRedirectUrlWithAccess(accessToken);
             } else {
-                loginSuccess(response, oAuth2User);
+                redirectUrl = loginSuccess(response, oAuth2User);
             }
+            response.setStatus(HttpServletResponse.SC_OK);
+            getRedirectStrategy().sendRedirect(request, response, redirectUrl);
         } catch (Exception e) {
             throw e;
         }
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private String makeRedirectUrlWithAccess(String accessToken) {
+        return UriComponentsBuilder.fromUriString(frontRedirectUri + accessToken)
+                .build()
+                .toUriString();
+    }
+
+    private String makeRedirectUrlWithAccessAndRefresh(String accessToken, String refreshToken) {
+        return UriComponentsBuilder.fromUriString(frontRedirectUri + accessToken + "_" + refreshToken)
+                .build()
+                .toUriString();
+    }
+
+    private String loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
         log.info("로그인 성공 및 토큰 생성");
         User user = findUserByEmail(oAuth2User.getEmail());
         String accessToken = jwtService.createAccessToken(user);
         String refreshToken = jwtService.createRefreshToken(user);
 
         log.info("USER access token : " + accessToken);
+        log.info("USER refresh token : " + accessToken);
 
-        response.addHeader(jwtService.getAccessTokenHeader(), "Bearer " + accessToken);
-        response.addHeader(jwtService.getRefreshTokenHeader(), "Bearer " + refreshToken);
-
-        response.setStatus(HttpServletResponse.SC_OK);
         jwtService.setAccessTokenHeader(response, accessToken);
         jwtService.setRefreshTokenHeader(response, refreshToken);
         user.updateRefreshToken(refreshToken);
         userRepository.saveAndFlush(user);
+
+        return makeRedirectUrlWithAccessAndRefresh(accessToken, refreshToken);
     }
 
     private User findUserByEmail(String email) {
