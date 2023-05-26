7679cf851cc80368dd7e365e3cf49965256f4b3a
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-26 17:16:46 +0900

    feat: add @NoArgsConstructor, @AllArgsConstructor

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/vote/UpdatePredictionRequest.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/vote/UpdatePredictionRequest.java
@@ -3,11 +3,15 @@ package com.cocktailmasters.backend.vote.controller.dto.vote;
 import com.cocktailmasters.backend.vote.controller.dto.item.PredictionDto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UpdatePredictionRequest {
 
