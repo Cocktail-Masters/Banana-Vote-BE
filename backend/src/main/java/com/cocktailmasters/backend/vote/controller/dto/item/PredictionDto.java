51ded9692fdb628cac28d37843b79997ec4536fc
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-26 17:16:57 +0900

    feat: add @AllArgsConstructor

--- backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/item/PredictionDto.java
+++ backend/src/main/java/com/cocktailmasters/backend/vote/controller/dto/item/PredictionDto.java
@@ -2,14 +2,20 @@ package com.cocktailmasters.backend.vote.controller.dto.item;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PredictionDto {
 
+    @NotNull
     private Long voteItemId;
+
+    @NotNull
     private Long points;
 }
