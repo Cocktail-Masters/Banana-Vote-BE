29ccd0db516eba62d36f179b80665169199fa107
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-27 00:19:55 +0900

    feat: modify deleteTag API to deleteTags API

--- backend/src/main/java/com/cocktailmasters/backend/common/contorller/TagController.java
+++ backend/src/main/java/com/cocktailmasters/backend/common/contorller/TagController.java
@@ -1,13 +1,18 @@
 package com.cocktailmasters.backend.common.contorller;
 
 import com.cocktailmasters.backend.common.contorller.dto.CreateTagsRequest;
+import com.cocktailmasters.backend.common.contorller.dto.DeleteTagsRequest;
 import com.cocktailmasters.backend.common.contorller.dto.FindTop10TagsResponse;
 import com.cocktailmasters.backend.common.service.TagService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import static com.cocktailmasters.backend.config.SwaggerConfig.SECURITY_SCHEME_NAME;
 
 @Tag(name = "tag", description = "태그 관리")
 @RequiredArgsConstructor
@@ -17,10 +22,12 @@ public class TagController {
 
     private final TagService tagService;
 
-    @Operation(summary = "DB에 태그 추가", description = "DB를 태그들 추가, 없는 태그 추가, 있는 태그 무시")
+    @Operation(summary = "DB에 태그 추가", description = "DB를 태그들 추가, 없는 태그 추가, 있는 태그 무시",
+            security = {@SecurityRequirement(name = SECURITY_SCHEME_NAME)})
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("")
-    public ResponseEntity<String> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
-        //TODO: 관리자 검증
+    public ResponseEntity<String> createTags(@RequestHeader(name = "Authorization", required = false) String token,
+                                             @RequestBody CreateTagsRequest createTagsRequest) {
         if (tagService.createTags(createTagsRequest))
             return ResponseEntity.created(null).build();
         else
@@ -34,13 +41,15 @@ public class TagController {
                 .body(tagService.findTop10Tags());
     }
 
-    @Operation(summary = "태그 삭제", description = "태그 삭제")
-    @DeleteMapping("/{tag_id}")
-    public ResponseEntity<String> deleteTag(@PathVariable("tag_id") Long tagId) throws Exception {
-        if (tagService.deleteTag(tagId)) {
+    @Operation(summary = "해당 단어가 포함된 모든 태그들 삭제", description = "DB에 있는 태그들 삭제, 없는 태그들은 무시",
+            security = {@SecurityRequirement(name = SECURITY_SCHEME_NAME)})
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @DeleteMapping("")
+    public ResponseEntity<String> deleteTags(@RequestHeader(name = "Authorization", required = false) String token,
+                                             @RequestBody DeleteTagsRequest deleteTagsRequest) {
+        if (tagService.deleteTags(deleteTagsRequest)) {
             return ResponseEntity.created(null).build();
         }
-        //TODO: 예외처리
-        throw new Exception();
+        return ResponseEntity.badRequest().build();
     }
 }
