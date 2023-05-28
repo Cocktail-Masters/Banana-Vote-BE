1b323326bb80d6856326acfa7d4546685827d68f
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-28 23:55:51 +0900

    feat: modify code convention

--- backend/src/main/java/com/cocktailmasters/backend/goods/controller/BadgeController.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/BadgeController.java
@@ -1,32 +1,23 @@
 package com.cocktailmasters.backend.goods.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.cocktailmasters.backend.goods.controller.dto.BadgeRequest;
 import com.cocktailmasters.backend.goods.controller.dto.BadgeResponse;
 import com.cocktailmasters.backend.goods.controller.dto.UserBadgeResponse;
 import com.cocktailmasters.backend.goods.service.BadgeService;
 import com.cocktailmasters.backend.goods.service.UserBadgeService;
-import static com.cocktailmasters.backend.config.SwaggerConfig.SECURITY_SCHEME_NAME;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.cocktailmasters.backend.config.SwaggerConfig.SECURITY_SCHEME_NAME;
 
 @Tag(name = "뱃지", description = "뱃지와 관련된 기능")
 @RequiredArgsConstructor
@@ -38,7 +29,7 @@ public class BadgeController {
     private final UserBadgeService userBadgeService;
 
     @Operation(summary = "뱃지 생성(관리자용)", description = "뱃지를 생성", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<String> createBadge(@RequestBody @Valid BadgeRequest badgeRequest) {
@@ -49,11 +40,11 @@ public class BadgeController {
     }
 
     @Operation(summary = "뱃지 수정(관리자용)", description = "뱃지를 수정", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{badgeId}")
     public ResponseEntity<String> modifiyBadge(@PathVariable Long badgeId,
-            @RequestBody @Valid BadgeRequest badgeRequest) {
+                                               @RequestBody @Valid BadgeRequest badgeRequest) {
         int res = badgeService.modifiyBadge(badgeId, badgeRequest);
         if (res == 1)
             return ResponseEntity.ok().build();
@@ -64,7 +55,7 @@ public class BadgeController {
     }
 
     @Operation(summary = "뱃지 삭제(관리자용)", description = "뱃지를 삭제", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{badgeId}")
     public ResponseEntity<String> deleteBadge(@PathVariable Long badgeId) {
@@ -93,7 +84,7 @@ public class BadgeController {
     }
 
     @Operation(summary = "유저가 소유 중인 뱃지 리스트 반환", description = "유저가 소유 중인 뱃지 리스트 반환 (로그인 필요)", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/users")
     public ResponseEntity<List<UserBadgeResponse>> getUsersBadgeList() {
@@ -108,11 +99,11 @@ public class BadgeController {
     }
 
     @Operation(summary = "유저에게 해당 뱃지를 추가(관리자용)", description = "해당 유저에게 뱃지를 추가, 유저가 없거나 추가할 뱃지가 없거나, \n해당 유저가 이미 그 뱃지를 가지고 있을 경우 X", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/users/{badgeId}")
     public ResponseEntity<String> addBadgeToUser(@PathVariable long badgeId,
-            @RequestParam(required = true, name = "userId") long userId) {
+                                                 @RequestParam(required = true, name = "userId") long userId) {
         int res = userBadgeService.addBadgeToUser(badgeId, userId);
 
         if (res == 1)
@@ -124,11 +115,11 @@ public class BadgeController {
     }
 
     @Operation(summary = "유저에게 해당 뱃지를 삭제(관리자용)", description = "해당 유저에게 뱃지를 삭제, 유저가 없거나 삭제할 뱃지가 없거나", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/users/{badgeId}")
     public ResponseEntity<String> deleteBadgeToUser(@PathVariable long badgeId,
-            @RequestParam(required = true, name = "userId") long userId) {
+                                                    @RequestParam(required = true, name = "userId") long userId) {
         if (userBadgeService.deleteBadgeFromUser(badgeId, userId))
             return ResponseEntity.ok().build();
         else
--- backend/src/main/java/com/cocktailmasters/backend/goods/controller/GoodsController.java
+++ backend/src/main/java/com/cocktailmasters/backend/goods/controller/GoodsController.java
@@ -1,20 +1,5 @@
 package com.cocktailmasters.backend.goods.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.cocktailmasters.backend.account.jwt.service.JwtService;
 import com.cocktailmasters.backend.goods.controller.dto.GoodsRequest;
 import com.cocktailmasters.backend.goods.controller.dto.GoodsResponse;
@@ -22,13 +7,18 @@ import com.cocktailmasters.backend.goods.controller.dto.UserGoodsResponse;
 import com.cocktailmasters.backend.goods.domain.GoodsType;
 import com.cocktailmasters.backend.goods.service.GoodsService;
 import com.cocktailmasters.backend.goods.service.UserGoodsService;
-import static com.cocktailmasters.backend.config.SwaggerConfig.SECURITY_SCHEME_NAME;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.cocktailmasters.backend.config.SwaggerConfig.SECURITY_SCHEME_NAME;
 
 @Tag(name = "상품", description = "상품과 관련된 기능 - 조회, 구매")
 @RequiredArgsConstructor
@@ -71,7 +61,7 @@ public class GoodsController {
     }
 
     @Operation(summary = "상품을 구매", description = "상품의 수량이 부족할 경우, 구매 일자가 유효하지 않을 경우 에러, 상품이 존재하지 않을 경우에도 에러(로그인 필요)", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @PostMapping("/{goodsId}")
     public ResponseEntity<String> buyGoods(
@@ -90,7 +80,7 @@ public class GoodsController {
     }
 
     @Operation(summary = "유저가 가진 상품 목록 조회", description = "유저가 가진 상품 목록 조회", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/users")
     public ResponseEntity<List<UserGoodsResponse>> getUsersGoodsList(
@@ -106,7 +96,7 @@ public class GoodsController {
     }
 
     @Operation(summary = "굿즈 생성(관리자용)", description = "굿즈 생성, 타입이 뱃지 일경우엔 X, 뱃지 생성 API 이용하세요", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<String> createGoods(@RequestBody @Valid GoodsRequest goodsRequest) {
@@ -125,7 +115,7 @@ public class GoodsController {
     }
 
     @Operation(summary = "굿즈 삭제(관리자용)", description = "굿즈 삭제", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{goodsId}")
     public ResponseEntity<String> deleteGoods(@PathVariable long goodsId) {
@@ -136,11 +126,11 @@ public class GoodsController {
     }
 
     @Operation(summary = "굿즈 수정(관리자용)", description = "굿즈 수정, 기입하지 않은 정보가 있을 시에 기본값으로 초기화에 주의", security = {
-            @SecurityRequirement(name = SECURITY_SCHEME_NAME) })
+            @SecurityRequirement(name = SECURITY_SCHEME_NAME)})
     @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{goodsId}")
     public ResponseEntity<String> modifyGoods(@PathVariable long goodsId,
-            @RequestBody @Valid GoodsRequest goodsRequest) {
+                                              @RequestBody @Valid GoodsRequest goodsRequest) {
         // date validation
         if (goodsRequest.getStartDate().isAfter(goodsRequest.getEndDate()))
             return ResponseEntity.badRequest().body("invalud date");
