17bd51e2b4838d6b0a8e7c1acc795140f39acf35
swoody1101@gmail.com <swoody1101@gmail.com>
2023-05-29 23:35:52 +0900

    feat: delete jwtTest API and create signIn, signOut, withdrawal, findUserInfo API

--- backend/src/main/java/com/cocktailmasters/backend/account/user/controller/UserController.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.cocktailmasters.backend.account.user.controller;
 
 import com.cocktailmasters.backend.account.jwt.service.JwtService;
 import com.cocktailmasters.backend.account.user.controller.dto.SignUpRequest;
-import com.cocktailmasters.backend.account.user.domain.entity.User;
 import com.cocktailmasters.backend.account.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,9 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 @Tag(name = "user", description = "회원 관리")
@@ -34,9 +32,30 @@ public class UserController {
         return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/jwt-test")
-    public String jwtTest(@RequestHeader(AUTHORIZATION) String token) {
-        User user = jwtService.findUserByToken(token);
-        return "jwtTest 요청 성공";
+    @Operation(summary = "일반 로그인", description = "일반 로그인")
+    @PostMapping("/sign-in")
+    public ResponseEntity<String> signIn() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @Operation(summary = "로그아웃", description = "로그아웃")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER', 'GUEST')")
+    @PostMapping("/sign-out")
+    public ResponseEntity<String> signOut() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER', 'GUEST')")
+    @DeleteMapping("")
+    public ResponseEntity<String> withdrawal() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @Operation(summary = "회원 정보 조회", description = "회원 정보 조회, " +
+            "다른 사람 조회 시 닉네임, 랭킹, 퍼센테이지 / 나의 회원 정보 조회 시 추가로 나이, 성별")
+    @GetMapping("/{user-id}")
+    public ResponseEntity<String> findUserInfo(@PathVariable("user-id") Long userId) {
+        return ResponseEntity.badRequest().build();
     }
 }
