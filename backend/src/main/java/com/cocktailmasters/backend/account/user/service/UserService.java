5e4e0d6b0978bc031d0f094aa23536bd62cd47fb
swoody1101@gmail.com <swoody1101@gmail.com>
2023-06-01 17:17:51 +0900

    feat: modify signIn, signOut, withdrawal, findUserInfo, updateNickname methods logic

--- backend/src/main/java/com/cocktailmasters/backend/account/user/service/UserService.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/user/service/UserService.java
@@ -1,8 +1,13 @@
 package com.cocktailmasters.backend.account.user.service;
 
+import com.cocktailmasters.backend.account.user.controller.dto.FindUserInfoResponse;
+import com.cocktailmasters.backend.account.user.controller.dto.SignInRequest;
 import com.cocktailmasters.backend.account.user.controller.dto.SignUpRequest;
+import com.cocktailmasters.backend.account.user.controller.dto.UpdateNicknameRequest;
 import com.cocktailmasters.backend.account.user.domain.entity.User;
 import com.cocktailmasters.backend.account.user.domain.repository.UserRepository;
+import com.cocktailmasters.backend.season.service.RankingService;
+import com.cocktailmasters.backend.util.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,18 +16,76 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final RankingService rankingService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public boolean signUp(SignUpRequest signUpRequest) throws Exception {
+    public boolean signUp(SignUpRequest signUpRequest) {
         if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent()) {
-            throw new Exception("This email already exists");
+            return false;
         }
         if (userRepository.findByNickname(signUpRequest.getNickname()).isPresent()) {
-            throw new Exception("This nickname already exists");
+            return false;
         }
         User user = signUpRequest.toUserEntity();
         user.encodePassword(passwordEncoder);
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean signIn(SignInRequest signInRequest) {
+        User user = userRepository.findByEmailAndIsActiveTrue(signInRequest.getEmail())
+                .orElse(null);
+        if (user == null) {
+            return false;
+        }
+        if (passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean signOut(User user) {
+        user.updateRefreshToken("");
+        return true;
+    }
+
+    public boolean withdrawal(User user) {
+        user.deleteUser();
+        userRepository.save(user);
+        return true;
+    }
+
+    public FindUserInfoResponse findUserInfo(User user, long findUserId) {
+        if (user != null && user.getId() == findUserId) {
+            return FindUserInfoResponse.builder()
+                    .nickname(user.getNickname())
+                    .equippedBadgeImageUrl(user.getEquippedBadgeImageUrl())
+                    .age(user.getAge())
+                    .gender(user.getGender())
+                    .ranking(rankingService.getCurrentSeasonUserRanking(user.getId()))
+                    .points(user.getPoints())
+                    .build();
+        }
+        User findUser = userRepository.findById(findUserId)
+                .orElse(null);
+        if (findUser == null) {
+            throw new NotFoundUserException();
+        }
+        return FindUserInfoResponse.builder()
+                .nickname(findUser.getNickname())
+                .equippedBadgeImageUrl(findUser.getEquippedBadgeImageUrl())
+                .ranking(rankingService.getCurrentSeasonUserRanking(findUser.getId()))
+                .build();
+    }
+
+    public boolean updateNickname(User user, UpdateNicknameRequest updateNicknameRequest) {
+        String newNickname = updateNicknameRequest.getNickname();
+        if (!userRepository.findByNickname(newNickname).isEmpty()) {
+            return false;
+        }
+        user.updateNickname(updateNicknameRequest.getNickname());
+        user.updateRoleGuestToUser();
         userRepository.save(user);
         return true;
     }
