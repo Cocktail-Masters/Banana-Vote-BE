ace361abf8aef48a6b8479fa69ccd303e5f93358
swoody1101@gmail.com <swoody1101@gmail.com>
2023-06-03 23:36:20 +0900

    feat: create VoteDto class

---
+++ backend/src/main/java/com/cocktailmasters/backend/account/user/controller/dto/item/VoteDto.java
@@ -0,0 +1,17 @@
+package com.cocktailmasters.backend.account.user.controller.dto.item;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class VoteDto {
+
+    private long id;
+    private String title;
+    private boolean isClosed;
+    private long predictedPoint;
+}
