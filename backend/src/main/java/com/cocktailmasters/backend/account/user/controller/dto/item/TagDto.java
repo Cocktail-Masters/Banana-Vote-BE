fdec6d9f61a93c975f3d7e914042868841c7518c
swoody1101@gmail.com <swoody1101@gmail.com>
2023-06-08 14:23:02 +0900

    feat: modify deprecated API PropertyNamingStrategy to PropertyNamingStrategies

--- backend/src/main/java/com/cocktailmasters/backend/account/user/controller/dto/item/TagDto.java
+++ backend/src/main/java/com/cocktailmasters/backend/account/user/controller/dto/item/TagDto.java
@@ -1,9 +1,12 @@
 package com.cocktailmasters.backend.account.user.controller.dto.item;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TagDto {
 
     @NotNull
