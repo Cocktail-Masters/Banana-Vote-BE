08cf7c4f89d335747ce5a5aa93e0cbdb59d15039
swoody1101@gmail.com <swoody1101@gmail.com>
2023-07-05 22:57:36 +0900

    style: modify code convention

--- backend/src/main/java/com/cocktailmasters/backend/picket/controller/dto/PicketRequest.java
+++ backend/src/main/java/com/cocktailmasters/backend/picket/controller/dto/PicketRequest.java
@@ -2,7 +2,6 @@ package com.cocktailmasters.backend.picket.controller.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
